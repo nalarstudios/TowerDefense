fd3f5e94c4805e99629192c471bd622571859c92
NitroTaker <nittrotaker@yahoo.com>
2019-07-31 15:12:51 -0700

    Tower Placer Dectection

---
+++ Assets/Scripts/TowerPlacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Placer"))
+        {
+            Vector3 screenSpaceMousePosition = Input.mousePosition;
+            Camera camera = Camera.main;
+            Ray ray = camera.ScreenPointToRay(screenSpaceMousePosition);
+            RaycastHit hit;
+            Vector3 worldPosition = new Vector3(0,0,0);
+            if (Physics.Raycast(ray, out hit))
+            {
+                worldPosition = hit.point;
+            }
+            print(worldPosition);
+        }
+    }
+}
