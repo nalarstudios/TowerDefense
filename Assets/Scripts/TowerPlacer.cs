edd0fd6e7d45db1868f7077f29e04f475cc45c66
NitroTaker <nittrotaker@yahoo.com>
2019-08-08 08:58:23 -0700

    Added a separate tower action for placing.

--- Assets/Scripts/TowerPlacer.cs
+++ Assets/Scripts/TowerPlacer.cs
@@ -2,29 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerPlacer : MonoBehaviour {
+public class TowerPlacer : MonoBehaviour
+{
+    public GameObject transparentTowerPrefab;
     public GameObject towerPrefab;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Placer"))
+	void Update ()
+    {
+        Vector3 screenSpaceMousePosition = Input.mousePosition;
+        Camera camera = Camera.main;
+        Ray ray = camera.ScreenPointToRay(screenSpaceMousePosition);
+        RaycastHit hit;
+        Vector3 worldPosition = new Vector3(0, 0, 0);
+        if (Physics.Raycast(ray, out hit))
         {
-            Vector3 screenSpaceMousePosition = Input.mousePosition;
-            Camera camera = Camera.main;
-            Ray ray = camera.ScreenPointToRay(screenSpaceMousePosition);
-            RaycastHit hit;
-            Vector3 worldPosition = new Vector3(0,0,0);
-            if (Physics.Raycast(ray, out hit))
-            {
-                worldPosition = hit.point;
-            }
-            print(worldPosition);
+            worldPosition = hit.point;
+        }
+        print(worldPosition);
 
+        if (Input.GetButtonDown("SelectTest"))
+        {
+            Instantiate(transparentTowerPrefab, worldPosition, Quaternion.identity);
+        }
+        else if (Input.GetButtonDown("Placer"))
+        {
             Instantiate(towerPrefab, worldPosition, Quaternion.identity);
         }
+              
     }
 }
