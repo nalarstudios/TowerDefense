b92440a6c118fdce01b5d984bcf173b22267495d
NitroTaker <nittrotaker@yahoo.com>
2019-08-27 09:08:57 -0700

    Detect Tower and Unplaceable Area

---
+++ Assets/Scripts/TowerPlacerCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacerCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Collision detected with trigger object " + other.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
