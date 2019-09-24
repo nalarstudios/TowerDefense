0ee81dc31c946aed26def3f140afa87c0ff303b0
Wesley Brown <nipponkid7@gmail.com>
2019-09-24 13:11:26 -0400

    Toggle Showing of Unplaceable Areas in the Editor
    This will allow developers to toggle the MeshRenderer
    component of all GameObjects tagged with "UnplaceableArea"
    on and off globally for easy scene editing.

---
+++ Assets/Scripts/Editor/UnplaceableAreaToggler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Globally toggles the MeshRenderer component all GameObjects tagged as "UnplaceableArea" to be on/off.
+/// </summary>
+static class UnplaceableAreaToggler
+{
+    private static bool meshRenderersAreEnabled = false;
+
+    [MenuItem("GameObject/Toggle Unplaceable Areas")]
+    private static void ToggleUnplaceableAreas()
+    {
+        GameObject[] unplaceableAreas = GameObject.FindGameObjectsWithTag("UnplaceableArea");
+        if (meshRenderersAreEnabled)
+        {
+            foreach (GameObject unplaceableArea in unplaceableAreas)
+            {
+                unplaceableArea.GetComponent<MeshRenderer>().enabled = false;
+            }
+            meshRenderersAreEnabled = false;
+        }
+        else
+        {
+            foreach (GameObject unplaceableArea in unplaceableAreas)
+            {
+                unplaceableArea.GetComponent<MeshRenderer>().enabled = true;
+            }
+            meshRenderersAreEnabled = true;
+        }
+    }
+}
