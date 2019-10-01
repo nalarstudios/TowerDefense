428999bfe0cc8d2393d096971f89fdf6e015c718
NitroTaker <nittrotaker@yahoo.com>
2019-10-01 09:51:29 -0700

    Added Enemy Spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemySpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 yLock = new Vector3(gameObject.transform.position.x, 0.3f, gameObject.transform.position.z);
+        Instantiate(enemySpawner, yLock, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
