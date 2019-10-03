728a9c8930d5a8ae1ea3a9f350f7f4eab937d098
NitroTaker <nittrotaker@yahoo.com>
2019-10-03 09:00:17 -0700

    Added enemy movement through waypoints.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.05f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+
+}
